2fb886b1159dd8a9c25f56c1d127bd7c7dfa35b3
rainerrodrigues <rainerrodrigues16@gmail.com>
2025-01-22 02:07:31 +0000

    Created procedure for user prompt

--- fpga_build.tcl
+++ fpga_build.tcl
@@ -7,4 +7,16 @@ set tool "Vivado" ;# Default tool
 set project_dir "./" ; # Default project directory
 set top_module "top" ; # Default top module name
 
+# Helper procedure for user prompts
+proc prompt_user {message default} {
+	puts -nonewline "$message (default: $default): "
+	flush stdout
+	gets stdin response
+	if {[string trim $response] eq ""} {
+		return $default
+	} else {
+		return $response
+	}
+}
+
 
